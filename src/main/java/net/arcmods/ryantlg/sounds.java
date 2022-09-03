188c8897372d8095169c6a11a66540d20e3f71e3
RyanTLG <78032176+RyanTLG@users.noreply.github.com>
2022-09-03 14:57:24 +0200

    bones

--- src/main/java/net/arcmods/ryantlg/sounds.java
+++ src/main/java/net/arcmods/ryantlg/sounds.java
@@ -5,20 +5,22 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class sounds {
-    
-    public static final Identifier SNIFF = new Identifier("gameritems:sniff");
-    public static SoundEvent SNIFF_SOUND_EVENT = new SoundEvent(SNIFF);
-    public static final Identifier MATT = new Identifier("gameritems:matt");
-    public static SoundEvent MATT_SOUND_EVENT = new SoundEvent(MATT);
-    public static final Identifier ELON = new Identifier("gameritems:elon");
-    public static SoundEvent ELON_SOUND_EVENT = new SoundEvent(ELON);
 
-    public static void register() {
-        gameritems.LOGGER.info("Sounds Loaded");
+    // THIS IS BROKEN DO NOT TOUCH
 
-        Registry.register(Registry.SOUND_EVENT, sounds.SNIFF, SNIFF_SOUND_EVENT); 
-        Registry.register(Registry.SOUND_EVENT, sounds.MATT, MATT_SOUND_EVENT); 
-        Registry.register(Registry.SOUND_EVENT, sounds.ELON, ELON_SOUND_EVENT); 
-    }
+    // public static final Identifier SNIFF = new Identifier("gameritems:sniff");
+    // public static SoundEvent SNIFF_SOUND_EVENT = new SoundEvent(SNIFF);
+    // public static final Identifier MATT = new Identifier("gameritems:matt");
+    // public static SoundEvent MATT_SOUND_EVENT = new SoundEvent(MATT);
+    // public static final Identifier ELON = new Identifier("gameritems:elon");
+    // public static SoundEvent ELON_SOUND_EVENT = new SoundEvent(ELON);
+
+    // public static void register() {
+    //     gameritems.LOGGER.info("Sounds Loaded");
+
+    //     Registry.register(Registry.SOUND_EVENT, sounds.SNIFF, SNIFF_SOUND_EVENT); 
+    //     Registry.register(Registry.SOUND_EVENT, sounds.MATT, MATT_SOUND_EVENT); 
+    //     Registry.register(Registry.SOUND_EVENT, sounds.ELON, ELON_SOUND_EVENT); 
+    // }
 
 }
